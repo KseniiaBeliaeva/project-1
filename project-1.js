afa6fef539e699b8b8739767052e8c8649c02974
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2017-08-31 14:04:38 +1000

    CSS changes for different screen sizes

--- project-1.js
+++ project-1.js
@@ -1,32 +1,28 @@
+// <!--TOP MENU SCRIPT -- >
+//=================================
+
+// let menuitem = '<div class="menu-item"><a href="';
+// let menuitem2 = '.html">';
+// let menuitemend = '</a></div>';
+// let menuarray = ['home', 'helpful FAQs', 'princess parties', 'superheroes parties', 'packages', 'make a booking / contacts', 'reviews'];
+// let menuarrayLinks = ['index', 'helpful-faqs', 'princess-parties', 'superheroes-parties', 'packages', 'make-a-booking', 'reviews'];
+// let sum = '';
+// for (let links = 0; links < menuarrayLinks.length; links++) {
+//     sum = sum + menuitem + menuarrayLinks[links] + menuitem2 + menuarray[links] + menuitemend;
+
+// }
+
+// document.getElementById('top-menu').innerHTML = sum;
 
 
 
 
-      // <!--TOP MENU SCRIPT -- >
-      //=================================
+// <!--SLIDESHOW SCRIPT IMAGES -- >
+//=================================
 
-    let menuitem = '<div class="menu-item"><a href="';
-    let menuitem2 = '.html">';
-    let menuitemend = '</a></div>';
-    let menuarray = ['home', 'helpful FAQs', 'princess parties', 'superheroes parties', 'packages', 'make a booking / contacts', 'reviews'];
-    let menuarrayLinks = ['index', 'helpful-faqs', 'princess-parties', 'superheroes-parties', 'packages', 'make-a-booking', 'reviews'];
-    let sum = '';
-        for (let links = 0; links < menuarrayLinks.length; links++) {
-            sum = sum + menuitem + menuarrayLinks[links] + menuitem2 + menuarray[links] + menuitemend;
-
-        }
-
-    document.getElementById('top-menu').innerHTML = sum;
-    
-
-
-
-    // <!--SLIDESHOW SCRIPT IMAGES -- >
-    //=================================
-    
-            let slideshowitem = '<div class="slideshow-image-wrapper" style="background-image:url(slideshowimages/';
-                let slideshowarray= ['slide1', 'slide2', 'slide3', 'slide4', 'slide5'];
-            let slideshowitemend = '.jpeg)"></div>';
+let slideshowitem = '<div class="slideshow-image-wrapper" style="background-image:url(slideshowimages/';
+let slideshowarray = ['slide1', 'slide2', 'slide3', 'slide4', 'slide5'];
+let slideshowitemend = '.jpeg)"></div>';
 let slidessum = '';
 for (let j = 0; j < slideshowarray.length; j++) {
 
@@ -40,50 +36,62 @@ document.getElementById('slideshow-container').innerHTML = slidessum;
 //=====================================
 
 
-    let slideIndex = 0;
-    showSlides();
-    function showSlides() {
-            let slides = document.getElementsByClassName('slideshow-image-wrapper');
-      for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-      slideIndex++;
-      if (slideIndex > slides.length) {
-            slideIndex = 1;
-        }
-
-      slides[slideIndex - 1].style.display = "flex";
-      slides[slideIndex - 1].style.justifyContent = "center";
-      slides[slideIndex - 1].style.height = "250px";
-      slides[slideIndex - 1].style.width = "1328px";
-
-
-
-      setTimeout(showSlides, 4000); // Change image every 4 seconds
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+    let slides = document.getElementsByClassName('slideshow-image-wrapper');
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "flex";
+    slides[slideIndex - 1].style.justifyContent = "center";
+    slides[slideIndex - 1].style.height = "250px";
+    slides[slideIndex - 1].style.width = "1328px";
+
+
+
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
 
 //HIDE MENU (3 BUTTONS FOR DIFFERENT AGES)
 //========================================
 
-    $(document).ready(function () {
-        $('.menu-icon').click(function () { 
-            $('.container').toggle('slow');
-        })
-    });
+// $(document).ready(function () {
+//     $('.menu-icon').click(function () { 
+//         $('.container').toggle('slow');
+//     })
+// });
 
-    $('.button-description').hide();
+$('.button-description').hide();
 $(document).ready(function () {
     $('button').click(function () {
         $(this).next().slideToggle();
 
     })
 });
-   
+
 //ANIMATED MENU BAR
 //=================
 
-    function myFunction(x) {
-        x.classList.toggle("change");
+function myFunction(x) {
+    x.classList.toggle("change");
 
     $('#top-menu').slideToggle();
-    }
+}
+
+$('.menu-icons').click(function () { 
+    $(this).animate({
+        backgroundColor: "red",
+        height: 100,
+    }, 1500);
+    $('.container').toggle(function (){ 
+        $(this).animate({
+            color:'red',
+        })
+    });
+    })
